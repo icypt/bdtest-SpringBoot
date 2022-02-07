18f31f372fdd4002d299e8be13d38720e68715ca
chenguojun <icypt_xy@126.com>
2022-02-07 23:25:32 +0800

    redis提交

--- SpringBoot-redis/src/main/java/com/bdtest/learn/controller/TestController.java
+++ SpringBoot-redis/src/main/java/com/bdtest/learn/controller/TestController.java
@@ -3,18 +3,37 @@ package com.bdtest.learn.controller;
 import com.bdtest.learn.entity.UserEntity;
 import com.bdtest.learn.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RedisTemplate<String, Serializable> redisTemplate;
+
 
 
     @GetMapping("/findUserByUserName")
     public UserEntity findUserByUserName(String userName){
         return userService.findUserByUserName(userName);
+    }
+
+    @GetMapping("/delete")
+    public String delete(String key) {
+        redisTemplate.delete(key);
+        return "ok";
+    }
+
+    @GetMapping("/expire")
+    public String expire(String key) {
+        redisTemplate.expire(key,10, TimeUnit.SECONDS);
+        return "ok";
     }
 }
--- SpringBoot-redis/src/main/java/com/bdtest/learn/service/impl/UserServiceImpl.java
+++ SpringBoot-redis/src/main/java/com/bdtest/learn/service/impl/UserServiceImpl.java
@@ -4,15 +4,30 @@ import com.bdtest.learn.entity.UserEntity;
 import com.bdtest.learn.mapper.UserMapper;
 import com.bdtest.learn.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private RedisTemplate<String, Serializable> redisTemplate;
+
+
     @Override
     public UserEntity findUserByUserName(String userName) {
-       return userMapper.findUserByUserName(userName);
+        String userKey = "userinfo:" + userName;
+        UserEntity userEntity;
+        if(redisTemplate.hasKey(userKey)) {
+            userEntity = (UserEntity) redisTemplate.opsForValue().get(userKey);
+        } else {
+            userEntity = userMapper.findUserByUserName(userName);
+            redisTemplate.opsForValue().set(userKey, userEntity);
+        }
+       return userEntity;
     }
 }
